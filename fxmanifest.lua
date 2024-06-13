44f9c1c2a26df42ef167294724877b33b754eafc
Tobs <93937426+t4by@users.noreply.github.com>
2024-06-13 23:24:31 +0100

    Updated Functions

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,11 +1,5 @@
 fx_version 'cerulean'
 game 'gta5'
-version '1.0.0'
 
--- Server script
+client_script 'client.lua'
 server_script 'ignite-teleport.lua'
-
--- Dependencies
-dependencies {
-    'qb-core'
-}
--- ignite-teleport.lua
+++ ignite-teleport.lua
@@ -1,35 +1,45 @@
--- Define your teleport coordinates
-local teleportCoords = {
-    police = vector4(425.1, -979.5, 30.7, 90.0), -- Example police coords
-    ems = vector4(298.3, -584.7, 43.3, 0.0), -- Example EMS coords
-    legion = vector4(215.8, -810.2, 30.7, 0.0), -- Example Legion coords
-    paleto = vector4(-449.7, 6012.9, 31.7, 45.0) -- Example Paleto coords
+local QBCore = exports['qb-core']:GetCoreObject()
+
+-- Define your Coords
+local locations = {
+    police = vector4(425.1, -979.5, 30.7, 90.0), -- Police Station coordinates
+    ems = vector4(295.8, -1446.9, 29.0, 49.5), -- EMS coordinates (example)
+    paleto = vector4(-448.4, 6012.6, 31.7, 45.0), -- Paleto coordinates (example)
+    legion = vector4(215.8, -810.1, 30.7, 70.0) -- Legion Square coordinates (example)
 }
 
 -- Function to teleport player
-local function teleportPlayer(source, coords)
-    local playerPed = GetPlayerPed(source)
-    if playerPed then
-        SetEntityCoords(playerPed, coords.x, coords.y, coords.z, false, false, false, true)
-        SetEntityHeading(playerPed, coords.w)
-        TriggerClientEvent('QBCore:Notify', source, 'Teleported successfully!', 'success')
+local function teleportPlayer(source, location)
+    local Player = QBCore.Functions.GetPlayer(source)
+    if Player then
+        if QBCore.Functions.HasPermission(source, "admin") or QBCore.Functions.HasPermission(source, "mod") or QBCore.Functions.HasPermission(source, "god") then
+            local coords = locations[location]
+            local ped = GetPlayerPed(source)
+            SetEntityCoords(ped, coords.x, coords.y, coords.z, false, false, false, true)
+            SetEntityHeading(ped, coords.w)
+            TriggerClientEvent('QBCore:Notify', source, "Teleported successfully!", "success")
+        else
+            TriggerClientEvent('QBCore:Notify', source, "You don't have permission to use this command!", "error")
+        end
     end
 end
 
--- Command registration function
-local function registerTeleportCommand(command, coordsKey)
-    QBCore.Commands.Add(command, 'Teleport to ' .. command .. ' location', {}, false, function(source, args)
-        local Player = QBCore.Functions.GetPlayer(source)
-        if Player.PlayerData.group == 'admin' then
-            teleportPlayer(source, teleportCoords[coordsKey])
-        else
-            TriggerClientEvent('QBCore:Notify', source, 'You do not have permission to use this command!', 'error')
-        end
-    end, 'admin')
-end
+-- Register the /police command
+QBCore.Commands.Add("police", "Teleport to the police station", {}, false, function(source, args)
+    teleportPlayer(source, "police")
+end)
 
--- Register each command
-registerTeleportCommand('police', 'police')
-registerTeleportCommand('ems', 'ems')
-registerTeleportCommand('legion', 'legion')
-registerTeleportCommand('paleto', 'paleto')
+-- Register the /ems command
+QBCore.Commands.Add("ems", "Teleport to the EMS station", {}, false, function(source, args)
+    teleportPlayer(source, "ems")
+end)
+
+-- Register the /paleto command
+QBCore.Commands.Add("paleto", "Teleport to Paleto", {}, false, function(source, args)
+    teleportPlayer(source, "paleto")
+end)
+
+-- Register the /legion command
+QBCore.Commands.Add("legion", "Teleport to Legion Square", {}, false, function(source, args)
+    teleportPlayer(source, "legion")
+end)
